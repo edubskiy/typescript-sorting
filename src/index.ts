ef6f62604c64ad03bb5303322ae1d7e7b802e501
Evgeniy <edubskiy@openwaygroup.com>
2020-02-07 20:18:33 +0300

    Added NumberCollections class as a way to unify sorting method with different collection structures

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,9 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-  sort(): void {
-    const { length } = this.collection;
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j+1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3 ,-5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log("New way sorting -> ", numbersCollection.data);
+ 
