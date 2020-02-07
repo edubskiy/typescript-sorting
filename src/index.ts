99a010d2f67e3471e11141703cb55ed716140935
Evgeniy <edubskiy@openwaygroup.com>
2020-02-07 20:35:23 +0300

    Added CharactersCollection sorter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection('Xaaa');
 
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log("New way sorting -> ", numbersCollection.data);
+const numberSorter = new Sorter(numbersCollection);
+const charactersSorter = new Sorter(charactersCollection);
+numberSorter.sort();
+charactersSorter.sort();
+console.log("New way sorting numbers -> ", numbersCollection.data);
+console.log("New way sorting characters -> ", charactersCollection.data);
  
